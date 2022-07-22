2b14878cdce357797f0a28b0119f277e838f3446
jparangdev <jparangdev@gmail.com>
2022-07-22 19:14:01 +0900

    git init

---
+++ src/main/java/kr/co/thirtytwodevs/cssselectortrainer/CssSelectorTrainerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.thirtytwodevs.cssselectortrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssSelectorTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssSelectorTrainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/thirtytwodevs/cssselectortrainer/CssSelectorTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.thirtytwodevs.cssselectortrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CssSelectorTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
