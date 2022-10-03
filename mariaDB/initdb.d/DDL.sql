63b4f2accbae20e6b111412938cd052a94d0bbf3
jparangdev <jparangdev@gmail.com>
2022-10-03 17:34:35 +0900

    chore(sql): DDL 수정

--- mariaDB/initdb.d/DDL.sql
+++ mariaDB/initdb.d/DDL.sql
@@ -1,16 +1,18 @@
 CREATE TABLE questions
 (
-	id        BIGINT AUTO_INCREMENT
-		PRIMARY KEY,
-	step      TINYINT                                NULL COMMENT '단계',
-	title     VARCHAR(128)                           NULL COMMENT '제목',
-	contents  TEXT                                   NULL COMMENT '내용',
-	solution  VARCHAR(256)                           NULL COMMENT '정답',
-	createdAt DATETIME   DEFAULT CURRENT_TIMESTAMP() NOT NULL COMMENT '생성일',
-	updatedAt DATETIME   DEFAULT CURRENT_TIMESTAMP() NOT NULL COMMENT '수정일',
-	deleted   TINYINT(1) DEFAULT 0                   NOT NULL COMMENT '삭제 여부'
+        id          BIGINT AUTO_INCREMENT
+                PRIMARY KEY,
+        step        TINYINT                                NULL COMMENT '단계',
+        title       VARCHAR(128)                           NULL COMMENT '제목',
+        contents    TEXT                                   NULL COMMENT '내용',
+        html        TEXT                                   NULL COMMENT 'HTML',
+        solution    VARCHAR(256)                           NULL COMMENT '정답',
+        description TEXT                                   NULL COMMENT '설명 또는 힌트',
+        createdAt   DATETIME   DEFAULT CURRENT_TIMESTAMP() NOT NULL COMMENT '생성일',
+        updatedAt   DATETIME   DEFAULT CURRENT_TIMESTAMP() NOT NULL COMMENT '수정일',
+        deleted     TINYINT(1) DEFAULT 0                   NOT NULL COMMENT '삭제 여부'
 )
-	COMMENT '문제 및 정답';
+        COMMENT '문제 및 정답';
 
 CREATE TABLE answers
 (
